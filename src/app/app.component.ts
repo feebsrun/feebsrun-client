15c0b1c1cfb2c56ae2a2cabe2cf23cdabe8c346f
sfebish <sjfebish@gmail.com>
2018-09-16 11:50:59 -0400

    missing color changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from './auth/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'run';
+  title = '';
 
   constructor(
     //updates: SwUpdate,
