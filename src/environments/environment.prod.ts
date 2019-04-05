7e8895611095f7b16968a36c5e087f478930ad83
sfebish <sjfebish@gmail.com>
2019-04-05 19:36:41 -0400

    fix prod build variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   auth: {
     clientID: '0TI9srD0bQoKmFJ4cnPK0fC26UKTWWyD',
     domain: 'febish.auth0.com', // e.g., you.auth0.com
-    audience: 'http://localhost:8080/api/', // e.g., http://localhost:3001
-    redirect: 'http://localhost:4200/authenticating',
+    audience: 'http://feebsrun.com/api/', // e.g., http://localhost:3001
+    redirect: 'http://feebsrun.com/authenticating',
     scope: 'openid profile email'
   }
 };
